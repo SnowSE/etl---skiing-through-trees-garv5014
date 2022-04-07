c1dd63471991e112c1b7add81fce5242b545ac20
Bridger Thompson <bridgerat00@gmail.com>
2022-04-07 16:26:24 -0600

    Added Specflow

---
+++ SkiiingTests/Features/Skiing.feature
@@ -0,0 +1,20 @@
+﻿Feature: Skiing
+
+A short summary of the feature
+
+@tag1
+Scenario: Read Tree Map
+	Given a file path TreeMap.txt
+	When we read it in
+	Then string array is not null
+
+Scenario: Loop Tree Map
+	Given the skier is at the end of the map
+	When they advances one more
+	Then they loop back to the front
+
+
+Scenario: Ski a Slope
+	Given a slope of 1:1
+	When the skier reaches the bottom
+	Then they hit 20 trees
---
+++ SkiiingTests/StepDefinitions/SkiingStepDefinitions.cs
@@ -0,0 +1,63 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace SkiiingTests.StepDefinitions
+{
+    [Binding]
+    public class SkiingStepDefinitions
+    {
+        [Given(@"a file path (.*)")]
+        public void GivenAFilePath(string p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"we read it in")]
+        public void WhenWeReadItIn()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"string array is not null")]
+        public void ThenStringArrayIsNotNull()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"the skier is at the end of the map")]
+        public void GivenTheSkierIsAtTheEndOfTheMap()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"they advances one more")]
+        public void WhenTheyAdvancesOneMore()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"they loop back to the front")]
+        public void ThenTheyLoopBackToTheFront()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"a slope of (.*)")]
+        public void GivenASlopeOf(Decimal p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"the skier reaches the bottom")]
+        public void WhenTheSkierReachesTheBottom()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"they hit (.*) trees")]
+        public void ThenTheyHitTrees(int p0)
+        {
+            throw new PendingStepException();
+        }
+    }
+}
